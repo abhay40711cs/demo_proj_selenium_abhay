e07cb58ea4145c78ce572382b39264753754bcc4
abhayk <abhayk@PC-ABHAYK.global.cadence.com>
2017-03-08 21:02:43 +0530

    Start Project -Commit Schema for Test class

--- src/proj_demo/Object_Class.java
+++ src/proj_demo/Object_Class.java
@@ -14,19 +14,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 	public void create_project()
 	{
 		
-		System.setProperty("webdriver.firefox.marionette","D:/16D/geckodriver-v0.14.0-win64/geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
-
-		driver.get("http://pvcon:team_pcb@vsrv-ecwrd04:8001");
-	    driver.manage().window().maximize();
-		driver.findElement(By.xpath("//span[@class='ms-cui-tt-span'][text()='Site']")).click();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.xpath("//a[@id='Ribbon.ECWMasterSiteTab.ECWMasterSiteTabGroup.NewProject-Large']")).click();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-
-		driver.switchTo().frame(driver.findElement(By.tagName("iframe']")));
-		
-		driver.quit();
 		
 		
 		}
